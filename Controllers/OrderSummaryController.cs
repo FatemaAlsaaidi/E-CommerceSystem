cc70401bf88e1d9aa213139bf0db4a9106a67a31
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-09-06 22:41:04 +0400

    create order summary controller

---
+++ Controllers/OrderSummaryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using E_CommerceSystem.Models;
+using E_CommerceSystem.Services;
+
+namespace E_CommerceSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderSummaryController : ControllerBase
+    {
+        private readonly IOrderSummaryService _svc;
+        public OrderSummaryController(IOrderSummaryService svc) => _svc = svc;
+
+        [Authorize(Roles = "Admin,Manager")]
+        [HttpGet("OrderSummary")]
+        public ActionResult<OrderSummaryDto> OrderSummary([FromQuery] DateTime fromUtc, [FromQuery] DateTime toUtc)
+            => Ok(_svc.GetSummary(fromUtc, toUtc));
+
+       
+    }
+}
